4cbf1d4ab803bbde0b28c10c5d677b70d0570faf
megychen <megy.chen203@gmail.com>
2018-11-02 21:25:11 +0800

    Implement product show page add_to_cart function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,12 +9,21 @@ class ProductsController < ApplicationController
 
   def add_to_cart
     @product = Product.find(params[:id])
+
+    if params[:quantity].present?
+      quantity = params[:quantity].to_i
+    else
+      quantity = 1
+    end
+
     if !current_cart.products.include?(@product)
-      current_cart.add_product_to_cart(@product)
+      current_cart.add_product_to_cart(@product, quantity)
     else
       @cart_item = current_cart.cart_items.find_by(product_id: params[:id])
-      @cart_item.quantity += 1
+      @cart_item.quantity += quantity
       @cart_item.save!
     end
+
+    render :json => { "success": true }
   end
 end
