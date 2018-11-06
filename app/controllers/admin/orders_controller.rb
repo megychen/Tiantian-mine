af7b29064f3b37ba502fb49cf1fc93d44ebc079a
megychen <megy.chen203@gmail.com>
2018-11-06 14:27:32 +0800

    Manager can pass or cancel the order

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,38 +3,45 @@ class Admin::OrdersController < ApplicationController
 
   before_action :authenticate_user!
   before_action :admin_required
+  before_action :find_order, except: [:index]
 
   def index
     @orders = Order.order("id DESC")
   end
 
   def show
-    @order = Order.find(params[:id])
     @product_lists = @order.product_lists
     @certificates = @order.certificates
   end
 
   def ship
-    @order = Order.find(params[:id])
     @order.ship!
     redirect_to admin_order_path(@order)
   end
 
   def shipped
-    @order = Order.find(params[:id])
     @order.deliver!
     redirect_to admin_order_path(@order)
   end
 
   def cancel
-    @order = Order.find(params[:id])
     @order.cancel_order!
     redirect_to admin_order_path(@order)
   end
 
   def return
-    @order = Order.find(params[:id])
     @order.return_good!
     redirect_to admin_order_path(@order)
+  end
+
+  def pay
+    @order.make_payment!
+    redirect_to admin_order_path(@order)
+  end
+
+  private
+
+  def find_order
+    @order = Order.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
         post :ship
         post :shipped
         post :return
+        post :pay
       end
     end
   end
