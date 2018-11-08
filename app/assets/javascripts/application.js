f159c4d62ab8ea23b46678d6c9e5815928eb0d2d
megychen <megy.chen203@gmail.com>
2018-11-08 17:19:53 +0800

    Add device adaption

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require activestorage
 //= require 'china_city/jquery.china_city'
-//= require bootstrap-sprockets
 //= require_tree .
