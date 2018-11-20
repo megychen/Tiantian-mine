580b9e9731d4018a0cb0504a601293a5f4782fbb
megychen <megy.chen203@gmail.com>
2018-11-20 13:49:58 +0800

    User can update info before order submmit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,18 +19,32 @@
 //= require_tree .
 
 $(document).on('turbolinks:load', function () {
+
+  /* ****************************
+
+    flash auto disappear
+
+  ***************************** */
+
   $(".alert").fadeOut(2000);
+
+
+  /* ****************************
+
+    register & login email address check
+
+  ***************************** */
 
   $(".registration").on("click", function() {
     if (!isValidEmailAddress()) {
-      modalMessage()
+      modalMessage("请输入有效的邮箱地址")
       return false
     }
   })
 
   $(".session").on("click", function() {
     if (!isValidEmailAddress()) {
-      modalMessage()
+      modalMessage("请输入有效的邮箱地址")
       return false
     }
   })
@@ -45,9 +59,167 @@ $(document).on('turbolinks:load', function () {
     return true
   }
 
-  function modalMessage() {
-    $(".modal-content").text("请输入有效的邮箱地址")
+  /* ****************************
+
+    modal message hint
+
+  ***************************** */
+
+  function modalMessage(msg) {
+    $(".modal-content").text(msg)
     $('#message').modal()
     $('#message').modal('show')
   }
+
+
+  /* ****************************
+
+    carts & checkout page product quantity button action
+
+  ***************************** */
+
+  var indexNumber, totalQuantity, quantityObj, quantityObjValue;
+
+  function sendQauntityChangeRequest (url, quantity) {
+    $.ajax({
+      url: url,
+      data: { quantity: quantity },
+      method: 'POST',
+      dataType: 'json', // 要求服务器回传 json
+      success: function (response) {
+        if (response.success !== true) {
+          alert(response.message)
+        }
+      }
+    })
+  }
+
+  // Calculate carts total price
+  function calculateCartsTotalPrice() {
+    var sum = 0
+    $('input[name="cart_item_id"]:checked').each(function () {
+      var cartItemId= $(this).attr("id")
+      var cartItemPrice = parseInt($("#price-" + cartItemId).text().split("￥")[1])
+      var quantity = parseInt($("#cart-item-quantity-" + cartItemId).val())
+      sum += cartItemPrice * quantity
+    })
+    $(".cart-item-total-price").text(sum)
+  }
+
+  // Calculate checkout total price
+  function calculateCheckoutTotalPrice() {
+    var sum = 0
+    $(".checkout-list").each(function () {
+      var flag = $(this).children().eq(0).attr("id")
+      var quantity = parseInt($("#cart-item-quantity-" + flag).val())
+      var price = parseInt($("#cart-item-product-price-" + flag).text())
+
+      sum += quantity * price
+    })
+
+    $(".checkout-item-total-price").text(sum)
+  }
+
+  $(".sub-btn").on("click", function (e) {
+    e.preventDefault()
+    indexNumber = $(this).data("indexNumber")
+    totalQuantity = $(document).find("#total-" + indexNumber).text()
+
+    quantityObj = $("#cart-item-quantity"+ '-' + indexNumber)
+    quantityObjValue = quantityObj.val()
+
+    if (parseInt(quantityObjValue) > 1) {
+      quantityObjValue = parseInt(quantityObjValue) - 1
+      quantityObj.val(quantityObjValue)
+      if (window.location.pathname === "/carts") {
+        calculateCartsTotalPrice()
+      } else {
+        calculateCheckoutTotalPrice()
+      }
+
+      var url = $(this).attr("href");
+      sendQauntityChangeRequest(url, quantityObjValue)
+    }
+
+  })
+
+  $(".inc-btn").on("click", function (e) {
+    e.preventDefault()
+    indexNumber =  $(this).data("indexNumber")
+    totalQuantity = parseInt($(document).find("#total-" +indexNumber).text())
+
+    quantityObj = $("#cart-item-quantity"+ '-' + indexNumber)
+    quantityObjValue = quantityObj.val()
+
+    if (parseInt(quantityObjValue) <= totalQuantity - 1) {
+      quantityObjValue = parseInt(quantityObjValue) + 1
+      quantityObj.val(quantityObjValue)
+      if (window.location.pathname === "/carts") {
+        calculateCartsTotalPrice()
+      } else {
+        calculateCheckoutTotalPrice()
+      }
+
+      var url = $(this).attr("href");
+      sendQauntityChangeRequest(url, quantityObjValue)
+    } else {
+      modalMessage("库存不足")
+    }
+  })
+
+  $(".cart-list .quantity-value").each(function () {
+    $(this).on("change", function () {
+      indexNumber = $(this).next().data("indexNumber")
+      totalQuantity = parseInt($(document).find("#" +indexNumber).text())
+      quantityObjValue = parseInt($(this).val())
+
+      if (window.location.pathname === "/carts") {
+        calculateCartsTotalPrice()
+      } else {
+        calculateCheckoutTotalPrice()
+      }
+
+      var url = $(this).next().attr("href")
+
+      if ( quantityObjValue < 1 ) {
+        $(this).val("1")
+        return
+      } else if (quantityObjValue > totalQuantity) {
+        $(this).val("1")
+        modalMessage("库存不足")
+        return
+      } else {
+        sendQauntityChangeRequest(url, quantityObjValue)
+      }
+    })
+  })
+
+  // 监听复选框的变化
+  $('input[name="cart_item_id"]').each(function () {
+    $(this).on("change", function(){
+      calculateCartsTotalPrice()
+    });
+  })
+
+  // 确认结账
+  $(".submit-checkout-btn").click(function (e) {
+    e.preventDefault()
+
+    var checkedItem = $('input[name="cart_item_id"]:checked')
+
+    if (checkedItem.length === 0) {
+      messageHint("您还没有选中商品")
+      return
+    }
+
+    var url = $(this).attr("href")
+    var cartItemId = []
+    checkedItem.each(function () {
+      cartItemId.push($(this).attr("id"))
+    })
+
+    url = '/carts/checkout?cart_item_ids=' + cartItemId
+
+    $(location).attr('href', url)
+  })
 })
