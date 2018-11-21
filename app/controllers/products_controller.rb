9951fbbb94ec331b0ca10d654a1ab760373270ca
megychen <megy.chen203@gmail.com>
2018-11-21 17:11:35 +0800

    User can see unpay order on index page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    @orders = current_user.orders.where(:is_confirmed => true, :is_paid => false).recent.limit(3)
   end
 
   def show
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def render_product_list_total_price(product_lists)
+    sum = 0
+    product_lists.each do |product_list|
+      sum += product_list.product_price * product_list.quantity
+    end
+    sum
+  end
 end
