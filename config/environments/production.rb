3cd94a043b73baa4df28805c241ced19ee49d425
megychen <megy.chen203@gmail.com>
2018-12-11 20:24:49 +0800

    Sendcloud config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'http://365tiantian.xyz'}
+  config.action_mailer.default_url_options = { :host => 'localhost:3000'}
 
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
